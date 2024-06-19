678fffc80945ea7d5bee95fcc51c139f9d3d0dd4
Diego Contreras <66972191+LilDie@users.noreply.github.com>
2024-06-19 14:30:50 -0400

    crecion de maquina falta ver el como eliminarlo de la collection machines

--- src/app/api/maquinas/[id]/route.ts
+++ src/app/api/maquinas/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/libs/mongodb";
 import Machine from "@/models/machine";
-
+import Room from "@/models/room";
 
 
 //GET A ONE MACHINE
@@ -90,3 +90,35 @@ export async function GET(request: any, { params: { id } }: any) {
       );
     }
   }
+
+
+ 
+export async function DELETE(request: Request, { params }: { params: { id_machine: string } }) {
+  try {
+    await connectDB(); // Conectar a la base de datos
+
+    // Eliminar la máquina de la colección `machines`
+    const deletedMachine = await Machine.findOneAndDelete({ id_machine : params.id_machine });
+
+    // Verificar si se encontró y eliminó la máquina
+    if (!deletedMachine) {
+      return NextResponse.json({ message: "Máquina no encontrada" }, { status: 404 });
+    }
+
+    // Actualizar las salas que contienen esta máquina en su lista `id_machine`
+    const roomsToUpdate = await Room.find({ id_machine: params.id_machine });
+
+    if (roomsToUpdate.length > 0) {
+      // Iterar sobre las salas que tienen esta máquina y actualizarlas
+      for (const room of roomsToUpdate) {
+        const updatedIdMachines = room.id_machine.filter(id => id !== params.id_machine);
+        await Room.findByIdAndUpdate(room._id, { id_machine: updatedIdMachines }, { new: true });
+      }
+    }
+
+    return NextResponse.json({ message: "Máquina eliminada con éxito" }, { status: 200 });
+  } catch (error) {
+    console.error("Error al eliminar la máquina:", error);
+    return NextResponse.json({ message: "Error al eliminar la máquina", error: error.message }, { status: 500 });
+  }
+}
