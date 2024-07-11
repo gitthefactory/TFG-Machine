d25c54b775065f41a2d93bc50788cbe603ed5e40
Janis Aravena <janisaravenaespinoza@gmail.com>
2024-07-11 14:03:43 -0600

    Los juegos se en control panel, ahora se muestran desde la juegosApi en PROCESO

--- src/app/dashboard/juegos/page.tsx
+++ src/app/dashboard/juegos/page.tsx
@@ -1,50 +1,21 @@
 "use client";
 
+import React from "react";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import AssignClients from "@/components/Juegos/AssignClients";
+import GameStatusHandler from "@/components/Juegos/gameStatusHandler"; 
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import getMachines from "@/controllers/getMachines";
-import React, { useEffect, useState } from "react";
 
-interface MaquinaData {
-  _id: string; // Añade la propiedad id a UsuarioData
-  nombre: string;
-  descripcion: string;
-  status: number;
-  acciones: JSX.Element; // Cambio de string a JSX.Element
-}
 
-const Maquinas: React.FC = () => {
-  const [maquinas, setMaquinas] = useState<MaquinaData[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getMachines("",5); // Llama a la función para obtener salas
-        // Añade un id único a cada usuario
-        const maquinasConIds = data.map((maquina: any, index: { toString: () => any; }) => ({
-          ...maquina,
-          id: index.toString(),
-        }));
-        setMaquinas(maquinasConIds);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-
-    fetchData();
-  }, []);
+const Juegos: React.FC = () => {
 
   return (
     <>
       <DefaultLayout>
         <Breadcrumb pageName="Control de Juegos" />
-        <AssignClients maquinas={maquinas} />
+        <GameStatusHandler />
       </DefaultLayout>
     </>
   );
 };
 
-export default Maquinas;
+export default Juegos;
--- src/controllers/juegos/getJuegos.ts
+++ src/controllers/juegos/getJuegos.ts
@@ -1,53 +1,18 @@
-import { getSession } from "next-auth/react";
-import fetch from 'node-fetch';
-
 const ITEMS_PER_PAGE = 6;
 
-export default async function getJuegos(userId: string, currentPage: number) {
+export default async function getJuegos(query: string, currentPage: number) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
-    // Obtener la sesión del usuario actual
-    const session = await getSession();
+    const response = await fetch(`/api/juegosApi?query=${query}&limit=${ITEMS_PER_PAGE}&offset=${offset}`, {
+      cache: "no-store",
+    });
 
-    if (!session) {
-      throw new Error('Usuario no autenticado.');
-    }
-
-    // Obtener la máquina asociada al usuario
-    const responseMaquinas = await fetch(`/api/maquinas/${userId}`);
-    const maquinaSeleccionada = await responseMaquinas.json();
-
-    if (!maquinaSeleccionada) {
-      throw new Error('La máquina seleccionada no existe para este usuario.');
-    }
-
-    // Verificar si el usuario es un administrador
-    const isAdmin = session.user?.typeProfile === '660ebaa7b02ce973cad66550';
-
-    if (isAdmin) {
-      // Si el usuario es administrador, obtener juegos de la API de juegos
-      const responseJuegos = await fetch(`/api/juegosApi?query=&limit=${ITEMS_PER_PAGE}&offset=${offset}`, {
-        cache: "no-store",
-      });
-      const juegos = await responseJuegos.json();
-      return juegos.data;
-    } else {
-      // Si el usuario no es administrador, obtener los juegos de la máquina seleccionada
-      const juegosMaquina = maquinaSeleccionada.games;
-      
-      // Filtrar los juegos del usuario para devolver solo aquellos con la propiedad checked establecida como true
-      const juegosUsuario = juegosMaquina.filter(juego => juego.checked === true);
-      
-      // Calcular el índice de inicio y el índice de finalización para la paginación
-      const startIndex = offset;
-      const endIndex = Math.min(startIndex + ITEMS_PER_PAGE, juegosUsuario.length);
-      
-      // Devolver los juegos del usuario dentro del rango de la página actual
-      return juegosUsuario.slice(startIndex, endIndex);
-    }
+    const games = await response.json();
+    return games.data;
   } catch (error) {
-    // Manejar el error según lo necesites
+    console.log(error);
+    // Aquí puedes manejar el error según lo necesites
     throw new Error('Error al obtener juegos');
   }
 }
