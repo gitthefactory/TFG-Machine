3af066fe7723deca2b2b52f495c5c7510e56ea57
Diego Contreras <66972191+LilDie@users.noreply.github.com>
2024-07-23 13:13:43 -0400

    config de Image/next

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@ const nextConfig = {
    distDir: 'build', 
    typescript: {
        ignoreBuildErrors: true,
-   }
+   },
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'factorygaming.s3.amazonaws.com',
+        pathname: '/**',    
+      },
+    ],
+  },
 };
 
 export default nextConfig;
