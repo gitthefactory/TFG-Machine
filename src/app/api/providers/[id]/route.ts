e93f5cc1cd4106f4f81b06cd0e76e1d327f170dd
Janis Aravena <janisaravenaespinoza@gmail.com>
2024-07-29 11:12:56 -0600

    cambio status de proveedores para prueba en prod

--- src/app/api/providers/[id]/route.ts
+++ src/app/api/providers/[id]/route.ts
@@ -37,40 +37,81 @@ export async function GET(request: any, { params: { id } }: any) {
 
   //PRUEBA DE PUT PARA PROVEEDORES
 
-  export async function PUT(request, { params: { id } }) {
+  // export async function PUT(request, { params: { id } }) {
+  //   try {
+  //     const { status } = await request.json(); // Solo obtenemos el campo 'status' del cuerpo JSON
+  
+  //     await connectDB();
+  
+  //     const provider = await Providers.findById(id);
+  
+  //     if (!provider) {
+  //       return NextResponse.json(
+  //         {
+  //           message: "Game not found",
+  //         },
+  //         { status: 404 }
+  //       );
+  //     }
+  
+  //     // Actualizamos el estado del juego
+  //     provider.status = status;
+  
+  //     const updatedGame = await provider.save();
+  
+  //     return NextResponse.json(
+  //       {
+  //         message: "Estado de provider actualizado con éxito",
+  //         data: updatedGame,
+  //       },
+  //       { status: 200 }
+  //     );
+  //   } catch (error) {
+  //     return NextResponse.json(
+  //       {
+  //         message: "Error al actualizar el estado del provider",
+  //         error: error.message,
+  //       },
+  //       {
+  //         status: 500,
+  //       }
+  //     );
+  //   }
+  // }
+
+  export async function PUT(request: any, { params: { id } }: any) {
     try {
-      const { status } = await request.json(); // Solo obtenemos el campo 'status' del cuerpo JSON
+      const {
+      
+        status,
+      
+
+      } = await request.json();
   
       await connectDB();
   
-      const provider = await Providers.findById(id);
+      const updatedMachineData = {
+       
+        status,
+      };
   
-      if (!provider) {
-        return NextResponse.json(
-          {
-            message: "Game not found",
-          },
-          { status: 404 }
-        );
-      }
+      const updatedMachine = await Providers.findByIdAndUpdate(id, updatedMachineData, { new: true });
   
-      // Actualizamos el estado del juego
-      provider.status = status;
-  
-      const updatedGame = await provider.save();
+      // Asegúrate de que id_machine esté incluido en la respuesta
+      // updatedMachine.id_machine = id_machine;
   
       return NextResponse.json(
         {
-          message: "Estado de provider actualizado con éxito",
-          data: updatedGame,
+          message: "Proveedor Actualizada con Éxito",
+          data: updatedMachine,
         },
         { status: 200 }
       );
     } catch (error) {
       return NextResponse.json(
         {
-          message: "Error al actualizar el estado del provider",
-          error: error.message,
+          message: "Error al actualizar Sala",
+          error,
         },
         {
           status: 500,
@@ -78,3 +119,4 @@ export async function GET(request: any, { params: { id } }: any) {
       );
     }
   }
+  
