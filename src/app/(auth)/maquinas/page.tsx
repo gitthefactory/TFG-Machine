7032fd72e96d698d4e47a4193366eed9fda5c49f
LilDie <neobad.reyes@gmail.com>
2024-08-29 15:36:35 -0400

    comprobación de status de maquinas, si no está habilitada es imposible logearse

--- src/app/(auth)/maquinas/page.tsx
+++ src/app/(auth)/maquinas/page.tsx
@@ -17,7 +17,6 @@ const Maquinas: React.FC = () => {
   });
 
   const [error, setError] = useState("");
-  // const [pending, setPending] = useState(false);
   const [loading, setLoading] = useState(false);
   const router = useRouter();
 
@@ -25,82 +24,106 @@ const Maquinas: React.FC = () => {
     setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   }
 
- 
-async function obtenerInformacionMaquina(idMachine) {
-  try {
-    const res = await axios.get(`/api/juegosApi/${idMachine}`);
-    
-    if (res.status === 200) {
-      const machineData = res.data.data;
-      const roomDetails = machineData.room;
-      return roomDetails;
-    } else {
-      throw new Error("Error al obtener la información de la máquina");
-    }
-  } catch (error) {
-    console.error("Error al obtener la información de la máquina:", error);
-    throw error;
-  }
-}
-
-
-async function handleSubmit(e: any) {
-  e.preventDefault();
-  if (!info.id_machine || !info.password) {
-    setError("Todos los campos son obligatorios");
-    return;
-  }
-
-  setLoading(true);
-  try {
-    console.log("Enviando solicitud al backend con los siguientes datos:", info);
-    const res = await signIn("credentialsmaquina", {
-      id_machine: info.id_machine,
-      password: info.password,
-      redirect: false,
-    });
-    console.log("Respuesta del backend:", res);
-
-    if (res?.error) {
-      setError("ID de máquina o contraseña incorrectos");
-    } else {
-      // Obtener información de la sala asociada a la máquina
-      const roomInfo = await obtenerInformacionMaquina(info.id_machine);
-      console.log("Detalles de la sala obtenidos:", roomInfo); // Agrega este console.log para verificar los detalles de la sala obtenidos
-
-      setTimeout(async () => {
-        const result = await Swal.fire({
-            title: 'CONFIRMACIÓN DE DATOS',
-            html: `
-                <p><span class="bold-text" style="color: black;">MAQUINA:</span> ${info.id_machine}</p>
-                <p><span class="bold-text" style="color: black;">SALA:</span> ${roomInfo.nombre}</p>
-                <p><span class="bold-text" style="color: black;">COMUNA:</span> ${roomInfo.comuna}</p>
-                <p><span class="bold-text" style="color: black;">UBICACIÓN:</span> ${roomInfo.ciudad} / ${roomInfo.pais}</p>
-            `,
-            icon: 'success',
-            showCancelButton: true,
-            confirmButtonColor: 'rgb(227, 17, 108)',
-            cancelButtonColor: 'rgb(102, 102, 102)',
-            confirmButtonText: 'SÍ, ESTÁN CORRECTOS',
-            cancelButtonText: 'CANCELAR',
-            customClass: {
-                title: 'custom-title',
-                htmlContainer: 'custom-html',
-            },
-        });
-      
-        if (result.isConfirmed) {
-          router.replace(`/games/?idMachine=${info.id_machine}`);
+  async function verificarEstadoMaquina(idMachine: string) {
+    try {
+      const res = await axios.get(`/api/maquinas`);
+      if (res.status === 200) {
+        const machines = res.data.data;
+        const machine = machines.find((m: any) => m.id_machine === idMachine);
+        if (machine && machine.status === 0) {
+          return false; // Máquina con status 0, no se debe permitir el login
         }
-        setLoading(false);
-      }, 1000);
+        return true; // Máquina con status distinto de 0, se puede proceder
+      } else {
+        throw new Error("Error al verificar el estado de la máquina");
+      }
+    } catch (error) {
+      console.error("Error al verificar el estado de la máquina:", error);
+      throw error;
     }
-  } catch (error) {
-    console.error("Ocurrió un error al manejar la respuesta del backend:", error);
-    setError("Ocurrió un error al iniciar sesión");
-    setLoading(false);
   }
-}
+
+  async function obtenerInformacionMaquina(idMachine: string) {
+    try {
+      const res = await axios.get(`/api/juegosApi/${idMachine}`);
+      if (res.status === 200) {
+        const machineData = res.data.data;
+        const roomDetails = machineData.room;
+        return roomDetails;
+      } else {
+        throw new Error("Error al obtener la información de la máquina");
+      }
+    } catch (error) {
+      console.error("Error al obtener la información de la máquina:", error);
+      throw error;
+    }
+  }
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    if (!info.id_machine || !info.password) {
+      setError("Todos los campos son obligatorios");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      // Verificar estado de la máquina antes de iniciar sesión
+      const estadoMaquina = await verificarEstadoMaquina(info.id_machine);
+      if (!estadoMaquina) {
+        setError("La máquina está inactiva. Por favor, contacte al administrador.");
+        setLoading(false);
+        return;
+      }
+
+      console.log("Enviando solicitud al backend con los siguientes datos:", info);
+      const res = await signIn("credentialsmaquina", {
+        id_machine: info.id_machine,
+        password: info.password,
+        redirect: false,
+      });
+      console.log("Respuesta del backend:", res);
+
+      if (res?.error) {
+        setError("ID de máquina o contraseña incorrectos");
+      } else {
+        // Obtener información de la sala asociada a la máquina
+        const roomInfo = await obtenerInformacionMaquina(info.id_machine);
+        console.log("Detalles de la sala obtenidos:", roomInfo);
+
+        setTimeout(async () => {
+          const result = await Swal.fire({
+              title: 'CONFIRMACIÓN DE DATOS',
+              html: `
+                  <p><span class="bold-text" style="color: black;">MAQUINA:</span> ${info.id_machine}</p>
+                  <p><span class="bold-text" style="color: black;">SALA:</span> ${roomInfo.nombre}</p>
+                  <p><span class="bold-text" style="color: black;">COMUNA:</span> ${roomInfo.comuna}</p>
+                  <p><span class="bold-text" style="color: black;">UBICACIÓN:</span> ${roomInfo.ciudad} / ${roomInfo.pais}</p>
+              `,
+              icon: 'success',
+              showCancelButton: true,
+              confirmButtonColor: 'rgb(227, 17, 108)',
+              cancelButtonColor: 'rgb(102, 102, 102)',
+              confirmButtonText: 'SÍ, ESTÁN CORRECTOS',
+              cancelButtonText: 'CANCELAR',
+              customClass: {
+                  title: 'custom-title',
+                  htmlContainer: 'custom-html',
+              },
+          });
+        
+          if (result.isConfirmed) {
+            router.replace(`/games/?idMachine=${info.id_machine}`);
+          }
+          setLoading(false);
+        }, 1000);
+      }
+    } catch (error) {
+      console.error("Ocurrió un error al manejar la respuesta del backend:", error);
+      setError("Ocurrió un error al iniciar sesión");
+      setLoading(false);
+    }
+  }
   
   return (
     <>
@@ -188,15 +211,7 @@ async function handleSubmit(e: any) {
             </div>
           </div>
         </div>
-        <footer>
-          <div className="footer-content">
-            <p>Para más información:</p>
-            <a href="https://THEFACTORYGAMING.COM/MACHINE">
-              <p>THEFACTORYGAMING.COM/MACHINE</p>
-            </a>
-          </div>
-        </footer>
-      </div> 
+      </div>
     </>
   );
 };
