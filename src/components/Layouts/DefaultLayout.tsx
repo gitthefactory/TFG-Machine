becd1f4cf1571e14585002e4e820339bb5a9837e
Carlos Romero <carlos.armandor28@gmail.com>
2024-09-16 15:44:50 -0300

    Loader

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -10,40 +10,27 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    let timer: NodeJS.Timeout;
-  
-    const startLoading = () => setLoading(true);
-    const stopLoading = () => setLoading(false);
-  
-    startLoading();
-    timer = setTimeout(stopLoading, 1000); 
-  
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
 
+  const sidebarWidth = 250; // Ancho del Sidebar
   return (
     <>
-      {loading && <Loader />}
-      <div className="flex h-screen overflow-hidden">
-        {/* Sidebar */}
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* Contenido */}
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          {/* Header */}
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          {/* Contenido principal */}
-          <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              {children}
-            </div>
-          </main>
+        <div className="flex h-screen overflow-hidden">
+          {/* Sidebar */}
+          <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          {/* Contenido */}
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            {/* Header */}
+            <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            {/* Contenido principal */}
+            <main>
+              <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                {children}
+              </div>
+            </main>
+          </div>
         </div>
-      </div>
+    
     </>
   );
 }
