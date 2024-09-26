9803ea1d3e569812d1e29d8e049fc19d77ec96b5
Carlos Romero <carlos.armandor28@gmail.com>
2024-09-26 13:04:50 -0300

    Botones de nuevos proveedores listos, falta que funcionen los juegos por parte de la agregadora

---
+++ src/components/game/PopOk.tsx
@@ -0,0 +1,202 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import getSessionData from "@/controllers/getSession";
+import GameUrl from '@/components/game/gameUrl';
+import Image from 'next/image';
+import { useSocket } from "@/app/api/socket/socketContext";
+import Swal from 'sweetalert2';
+import { signOut } from 'next-auth/react';
+
+interface Game {
+  id: number;
+  name: string;
+  category: string;
+  provider_name: string;
+  image: string;
+  status: number;
+}
+
+const Belatra: React.FC = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [selectedGame, setSelectedGame] = useState<Game | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [idMachine, setIdMachine] = useState<string | null>(null);
+  const [machineStatus, setMachineStatus] = useState<number | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const swiperRef = useRef<any>(null);
+  const [idMachineFromURL, setIdMachineFromURL] = useState<string | null>(null);
+  const { socket } = useSocket();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const sessionData = await getSessionData();
+        if (!sessionData || sessionData.status !== 200) {
+          console.error("Datos de sesión inválidos:", sessionData);
+          setLoading(false);
+          return;
+        }
+
+        const params = new URLSearchParams(window.location.search);
+        const idMachine = params.get('idMachine');
+        setIdMachineFromURL(idMachine);
+
+        if (idMachine) {
+          const machineResponse = await fetch(`/api/maquinas`);
+          const machineData = await machineResponse.json();
+          const machine = machineData.data.find((m: any) => m.id_machine === idMachine);
+
+          if (machine) {
+            setMachineStatus(machine.status);
+
+            if (machine.status === 0) {
+              setTimeout(async () => {
+                const result = await Swal.fire({
+                  title: 'ALERTA',
+                  html: `<p><span class="bold-text" style="color: black;">SU MAQUINA HA SIDO DESHABILITADA :</span></p>`,
+                  icon: 'warning',
+                  showCancelButton: false,
+                  confirmButtonColor: 'rgb(227, 17, 108)',
+                  confirmButtonText: 'ACEPTAR',
+                  customClass: {
+                    title: 'custom-title',
+                    htmlContainer: 'custom-html',
+                  },
+                });
+
+                if (result.isConfirmed) {
+                  await signOut({ callbackUrl: '/' });
+                }
+                setLoading(false);
+              }, 1000);
+
+              return;
+            }
+          } else {
+            console.warn("No se encontró la máquina con el ID proporcionado.");
+          }
+        }
+
+        const provider = 88;
+        const response = await fetch(`/api/juegosApi/${idMachine}/${provider}`);
+        const data = await response.json();
+
+        if (data.data?.token) {
+          setToken(data.data.token);
+        } else {
+          console.error("Token no disponible en la respuesta.");
+          setLoading(false);
+          return;
+        }
+
+        const globalGamesResponse = await fetch('/api/juegosApi');
+        const globalGamesData = await globalGamesResponse.json();
+
+        if (Array.isArray(globalGamesData.data)) {
+          const activeGlobalGames = globalGamesData.data.flatMap(providerData => providerData.games).filter(game => game.status === 1);
+          const activePopOkGames = data.data.games.filter((game: any) => game.maker === 'popok' && game.status === 1);
+
+          const finalPopOkGames = activePopOkGames.filter(popokGame =>
+            activeGlobalGames.some(globalGame => globalGame.id === popokGame.id)
+          );
+
+          setGames(finalPopOkGames);
+        } else {
+          console.error("Estructura de datos inesperada:", globalGamesData);
+        }
+
+        setLoading(false);
+      } catch (error) {
+        console.error("Error al obtener los datos de sesión:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    if (socket) {
+      const handleGameStatusUpdated = (gameStatusChange: Game) => {
+        console.log('Evento gameStatusUpdated recibido:', gameStatusChange);
+        setGames(prevGames => {
+          const updatedGames = prevGames.map(game =>
+            game.id === gameStatusChange.id ? { ...game, status: gameStatusChange.status } : game
+          );
+          return updatedGames;
+        });
+      };
+
+      socket.on('gameStatusUpdated', handleGameStatusUpdated);
+
+      return () => {
+        socket.off('gameStatusUpdated', handleGameStatusUpdated);
+      };
+    }
+  }, [socket]);
+
+  const handlePrevButtonClick = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+  const handleNextButtonClick = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
+  const handleGameClick = (game: Game) => {
+    setSelectedGame(game);
+  };
+
+  const closeGameUrl = () => {
+    setSelectedGame(null);
+  };
+
+  const filteredGames = games.filter(game => game.status === 1);
+
+  return (
+    <div className="belatra-container">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <div className="navigation-buttons">
+            <div className="swiper-button-prev swiper-button-prev-img" onClick={handlePrevButtonClick}></div>
+            <div className="swiper-button-next swiper-button-next-img" onClick={handleNextButtonClick}></div>
+          </div>
+          <Swiper slidesPerView={1} spaceBetween={10} ref={swiperRef}>
+            {[...Array(Math.ceil(filteredGames.length / 8))].map((_, pageIndex) => (
+              <SwiperSlide key={pageIndex}>
+                <div className="swiper-slide-content">
+                  {filteredGames.slice(pageIndex * 8, (pageIndex + 1) * 8).map((game, index) => (
+                    <div key={index} className="col-3 col-md-3">
+                      <div className="btn-game" onClick={() => handleGameClick(game)}>
+                        <Image
+                          src={game.image}
+                          alt={game.name}
+                          style={{ width: '100%' }}
+                          width={500}
+                          height={500}
+                        />
+                        <div className="subtitle">
+                          {game.name}
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {selectedGame && token && (
+            <GameUrl game={selectedGame} token={token} idMachine={idMachineFromURL} onClose={closeGameUrl} />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Belatra;
--- src/components/game/buttons.tsx
+++ src/components/game/buttons.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Bgaming from './bgaming';
 import Belatra from './belatra';
 import Aspect from "./aspect";
+import Booming from './Booming';
+import Popok from './PopOk';
 
 interface ButtonsProps {
   onSelectProvider: (provider: { id: number, provider_name: string }) => void;
@@ -11,12 +13,16 @@ const Buttons: React.FC<ButtonsProps> = ({ onSelectProvider }) => {
   const bgamingProps = { id: 68, provider_name: "Bgaming" };
   const belatraProps = { id: 29, provider_name: "Belatra" };
   const aspectProps = { id: 87, provider_name: "Aspect" };
+  const boomingPros = { id: 12, provider_name: "Booming Games" };
 
   return (
     <div>
       <Bgaming onClick={() => onSelectProvider(bgamingProps)} />
       <Belatra onClick={() => onSelectProvider(belatraProps)} />
       <Aspect onClick={() => onSelectProvider(aspectProps)} />
+      <Booming onClick={() => onSelectProvider(boomingPros)}/>
+      <Popok onClick={() => onSelectProvider(boomingPros)}/>  
+      
     </div>
   );
 }
